15fc87c11fb41d10a704427472a271ba521d0aad
habib-sc <habib97.cs@gmail.com>
2022-03-26 04:39:44 +0600

    Fix choose form cart

--- src/components/Cart/RandomCartItem/RandomCartItem.js
+++ src/components/Cart/RandomCartItem/RandomCartItem.js
@@ -4,12 +4,16 @@ import React from 'react';
 const RandomCartItem = (props) => {
     const { randomCartItem } = props;
     return (
-        <div className='cart-item'>
-            <img src={randomCartItem.image} alt="img" />
-            <h4>{randomCartItem.name}</h4>
-            <button>
-                <TrashIcon></TrashIcon>
-            </button>
+        <div>
+            {Object.keys(randomCartItem).length > 0 &&
+                <div className='cart-item'>
+                    <img src={randomCartItem?.image} alt="img" />
+                    <h4>{randomCartItem?.name}</h4>
+                    <button>
+                        <TrashIcon></TrashIcon>
+                    </button>
+                </div>
+            }
         </div>
     );
 };
